7aece9c8d8dc98c710869294946ad6430b22684e
Bruno Santos <brunohqsantos@hotmail.com>
2020-08-03 13:46:41 -0300

    Global styles

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
