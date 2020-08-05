106c2d4fa034a8b089e5c209216ba474602e0413
Bruno Santos <brunohqsantos@hotmail.com>
2020-08-05 18:11:27 -0300

    List teachers

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,27 +4,39 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps{
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
   return (
     <article className="teacher-item">
           <header>
-            <img src="https://avatars2.githubusercontent.com/u/60053139?s=460&v=4" alt="perfil"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Mussum Ipsum</strong>
-              <span>cacilds</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header> 
+
           <p>
-          Mussum Ipsum, cacilds vidis litro abertis. Diuretics paradis num copo é motivis de denguis. 
-          <br /><br />
-          Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. 
-          Aenean sit amet nisi. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
+          {teacher.bio}
           </p>       
 
           <footer>
             <p>
               Preço/hora 
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost},00</strong>
             </p>
             <button type="button">
               <img src={whatsappIcon} alt="whatsapp" />
