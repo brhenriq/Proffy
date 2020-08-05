585588b7c35828be6e1bbb1bdf3e442610d3aec0
Bruno Santos <brunohqsantos@hotmail.com>
2020-08-05 11:14:05 -0300

    Layout Teacher Form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,81 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+            <Input
+              label="Nome Completo"
+              name="name"
+            />
+
+            <Input
+              label="Avatar"
+              name="avatar"
+            />
+
+            <Input
+              label="Whatsapp"
+              name="whatsapp"
+            />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+            <Input
+              label="Matéria"
+              name="subject"
+            />
+
+            <Input
+              label="Custo por aula"
+              name="cost"
+            />
+        </fieldset>
+
+        <fieldset>
+          <legend>Horários disponíveis</legend>
+            <Input
+              label="Dia da semana"
+              name="week_day"
+            />
+
+            <Input
+              label="Das"
+              name="from"
+              type="time"
+            />
+
+            <Input
+              label="Até"
+              name="to"
+              type="time"
+            />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -10,25 +11,26 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+          <Input
+            label="Matéria"
+            name="subject"
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Input
+            label="Dia da semana"
+            name="week_day"
+          />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input
+            label="Hora"
+            name="time"
+            type="time"
+          />
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem/>
+        <TeacherItem />
       </main>
     </div>
   );
