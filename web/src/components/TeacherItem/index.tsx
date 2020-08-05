7be6bb0b05f9e4e6b21259b7510fc4772be3e8e6
Bruno Santos <brunohqsantos@hotmail.com>
2020-08-05 18:24:06 -0300

    Whatsapp message

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,12 @@ interface TeacherItemProps{
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
+  function createNewConnection(){
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
           <header>
@@ -38,10 +45,14 @@ const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
               Preço/hora 
               <strong>R$ {teacher.cost},00</strong>
             </p>
-            <button type="button">
+            <a 
+              href={`https://wa.me/${teacher.whatsapp}`} 
+              onClick={createNewConnection}
+              target="_blank"
+            >
               <img src={whatsappIcon} alt="whatsapp" />
               Entrar em contato
-            </button>
+            </a>
           </footer>  
         </article> 
   );
